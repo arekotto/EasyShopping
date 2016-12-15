89ab6f0b52fcca4b6c9970fd8c9f5afb3c78f5ea
Radek Potyka <rpotyka@milnerbrowne.com>
2016-12-15 20:13:44 +0100

    Request test class.

--- src/main/java/com/easydevs/controller/TestController.java
+++ src/main/java/com/easydevs/controller/TestController.java
@@ -3,16 +3,20 @@ package com.easydevs.controller;
 import com.easydevs.user.StandardUser;
 import com.easydevs.user.User;
 import com.easydevs.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
 
 /**
  * Created by arekotto on 15/12/2016.
  */
 @Controller
+@RequestMapping("/test")
 public class TestController {
 
     // just random stuff for testing
@@ -20,18 +24,34 @@ public class TestController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/create/{userId}")
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
     @ResponseBody
-    public String createUser(@PathVariable Integer userId) {
-        User user = new StandardUser(userId, "", "", "");
+    public void createUser(@RequestBody User user) {
         userService.createUser(user);
-        return "Created user with id: " + userId;
     }
 
-    @RequestMapping("/get/{userId}")
+    @RequestMapping(path = "/{userId}", method = RequestMethod.GET)
     @ResponseBody
-    public String getUser(@PathVariable Integer userId) {
+    public User getUser(@PathVariable Integer userId) {
         User user = userService.getUser(userId);
-        return "Found user: " + user;
+        return user;
+    }
+
+    @RequestMapping(path = "/{userId}", method = RequestMethod.POST)
+    @ResponseBody
+    public void updateUser(@PathVariable Integer userId, @RequestBody User user) {
+        userService.updateUser(user);
+    }
+
+    @RequestMapping(path = "/test-user", method = RequestMethod.GET)
+    @ResponseBody
+    public void tesUserCreate() {
+        User testUser = new StandardUser(0, "Test", "Testowy", "123");
+        this.createUser(testUser);
+        User user = this.getUser(new Integer(0));
+        log.info(user.getLogin(), user.getId(), user.getName());
+        userService.changeUserLogin(0, "Testowy2");
     }
 }
