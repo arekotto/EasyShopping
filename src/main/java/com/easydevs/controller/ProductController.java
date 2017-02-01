7b2db1479e43a47381370704b351ed440528c6bb
Radek Potyka <rpotyka@milnerbrowne.com>
2017-02-01 17:48:36 +0100

    Fix no user id bug.

--- src/main/java/com/easydevs/controller/ProductController.java
+++ src/main/java/com/easydevs/controller/ProductController.java
@@ -136,7 +136,7 @@ public class ProductController {
 
     @RequestMapping("/view/{productId}")
     public String view(Model model, @PathVariable Long productId, HttpServletRequest request,
-                       @CookieValue(value = "id", defaultValue = "") String userIdCookie) {
+                       @CookieValue(value = "id", defaultValue = "0") String userIdCookie) {
 
         StandardProduct product = (StandardProduct) productService.getProductById(productId);
         if (product != null) {
@@ -161,7 +161,7 @@ public class ProductController {
             }
 
             Boolean isReviewedByUser = product.isReviewedByUserId(userId);
-            if (isReviewedByUser) {
+            if (isReviewedByUser || userId == 0) {
                 model.addAttribute("isReviewed", true);
             } else {
                 model.addAttribute("isReviewed", false);
