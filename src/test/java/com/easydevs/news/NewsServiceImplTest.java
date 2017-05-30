0f332ce8c0f790116f3120d75a33c7e532dc9a32
Radek Potyka <rpotyka@milnerbrowne.com>
2017-05-30 23:15:18 +0200

    Implement tests for inserting/deleting products. Implement test for creating news and deleting user.

--- src/test/java/com/easydevs/news/NewsServiceImplTest.java
+++ src/test/java/com/easydevs/news/NewsServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.easydevs.news;
 
+import com.mongodb.Mongo;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -19,14 +23,40 @@ public class NewsServiceImplTest {
     @Autowired
     NewsService newsService;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @Before
     public void cleanUpDb(){
-        // TODO clean up db
+        mongoTemplate.dropCollection(News.class);
+        mongoTemplate.dropCollection("newsIdSequence");
     }
 
     @Test
     public void someTest() {
         newsService.findAll();
         assertEquals(3, 3);
+    }
+
+    @Test
+    public void insertNewsTest() {
+        News news = new News();
+
+        String title = "title";
+        String content = "content";
+        String author = "author";
+
+        news.setAuthor(author);
+        news.setContent(content);
+        news.setTitle(title);
+
+        newsService.insertNews(news);
+
+        List<News> newsList = newsService.findAll();
+        News testNews = newsList.get(0);
+
+        assertEquals(testNews.getAuthor(), author);
+        assertEquals(testNews.getTitle(), title);
+        assertEquals(testNews.getContent(), content);
     }
 }
